baad1af84b2c84283417dc95e839d516abb39116
Diogo Soares Ferreira <diogodsdf@hotmail.com>
2020-07-18 20:38:55 -0300

    add config service

--- config/services.php
+++ config/services.php
@@ -29,5 +29,10 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'google' => [
+        'cliente_id' => env('GOOGLE_APP_ID'),
+        'cliente_secret' => env('GOOGLE_APP_SECRET'),
+        'redirect' => env('GOOGLE_APP_CALLBACK'),
+    ],
 
 ];
